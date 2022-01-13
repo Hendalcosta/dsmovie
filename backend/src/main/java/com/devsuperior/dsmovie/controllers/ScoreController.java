8637e0c97971f7b5a8b75ab4a6cb4012d393be87
hendalcosta <hendalcosta@gmail.com>
2022-01-13 12:40:36 -0300

    Save score_Corrigido

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -17,7 +17,7 @@ public class ScoreController {
 	@Autowired
 	private ScoreService service;
 	
-	@PutMapping(value = "/{id}")
+	@PutMapping
 	public MovieDTO saveScore(@RequestBody ScoreDTO dto){
 		MovieDTO movieDTO = service.saveScore(dto);
 		return movieDTO;
